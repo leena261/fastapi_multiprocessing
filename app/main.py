2b1deb588399998ee550cf40d2dc907e7fdfefd1
Leena <121568943+leena261@users.noreply.github.com>
2024-06-03 15:40:35 +0530

    Update main.py
    Added Logger

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from app.models import AdditionRequest, AdditionResponse
 import multiprocessing
 from multiprocessing import Pool
 from utils.utils import get_cur_time
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='application.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = FastAPI()
 
@@ -11,13 +15,16 @@ app = FastAPI()
 def add_num_task(payload: List) -> dict:
     try:
         res = sum(payload)
+        logger.info(f"Success: add_num_task")
         return res
     except Exception as e:
+        logger.error(f"Error in add_num_task: {str(e)}")
         return None
     
 
 @app.post("/add_process")
 async def add_process(request: AdditionRequest) -> AdditionResponse :
+    logger.info(f"Process Started")
     p = Pool(processes = 2)
     starttime = get_cur_time()
     task = p.map_async(add_num_task, request.payload)
@@ -31,7 +38,6 @@ async def add_process(request: AdditionRequest) -> AdditionResponse :
         "started_at": starttime,
         "completed_at": endtime
         }
+    logger.info(f"Process Done: {add_resp}")
     return add_resp
     
-
-
